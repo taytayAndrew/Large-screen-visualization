1f1768409987b9591ab807bfab2e7998fcfa26fa
taytay <1273724496@qq.com>
2023-07-13 17:26:07 +0800

    完成head

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './shared/style.scss';
 import App from './app';
-
+import './assets/iconfont/iconfont.css'
 
 ReactDOM.render(
   <App/>,
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.scss';
 import imgBg from '../img/header.png'
+import '../assets/iconfont/iconfont.css'
 
+const updateDateTime = () => {
+  var now = new Date();
+  document.getElementById('time').innerHTML = now.toLocaleTimeString();
+  document.getElementById('date').innerHTML = now.toDateString()
+}
 export const Home = () => {
+  const a = useEffect(() => 
+  {
+    setInterval(updateDateTime, 1000);
+  },[]);
   return (
     <div className='home'>
-      <header style={{backgroundImage:`url(${(imgBg)})`}} />
+      <header>
+        <div className='time' text-white >
+          <div id='time'></div>
+          <div id='date'></div>
+        </div>
+        <div className='title' style={{backgroundImage:`url(${(imgBg)})`}}>
+        </div>
+        <div className='weather'>
+          <h3>深圳</h3>
+            <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-weather"></use>
+            </svg>
+        </div>
+      </header>
       <main>
-        <section className="section1"></section>
-        <section className="section2"></section>
-        <section className="section3"></section>
-        <section className="section4"></section>
-        <section className="section5"></section>
+        <section className="border section1">
+          <div className='governTotal'>
+            案发
+          </div>
+        </section>
+        <section className="border section2"></section>
+        <section className="border section3"></section>
+        <section className="border section4"></section>
+        <section className="border section5"></section>
       </main>
     </div>
     
