6bdad8b17219f1b3aabae1b367db2713f2d6919b
taytay <1273724496@qq.com>
2023-07-13 11:47:05 +0800

    完成整体的布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './home.scss';
+import imgBg from '../img/header.png'
 
 export const Home = () => {
   return (
-    <div>
-      <div className='x'>你好</div>
+    <div className='home'>
+      <header style={{backgroundImage:`url(${(imgBg)})`}} />
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
+    
   );
 };
