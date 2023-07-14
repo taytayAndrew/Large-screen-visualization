56f8c5ec082488045b4cd5622e9066008e0e9cc2
taytay <1273724496@qq.com>
2023-07-14 09:24:00 +0800

    完成第一个柱形图

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './shared/style.scss';
 import App from './app';
-import './assets/iconfont/iconfont.css'
+import './assets/iconfont/iconfont.js'
 
 ReactDOM.render(
   <App/>,
